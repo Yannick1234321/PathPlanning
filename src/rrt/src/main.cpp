96906135d256d354e1c8e1964097891569a5f5c3
jiangkai <646398793@qq.com>
2024-08-17 06:38:02 +0800

    生成路径，并发布至ros节点

--- src/rrt/src/main.cpp
+++ src/rrt/src/main.cpp
@@ -1,12 +1,14 @@
 #include "rrt.h"
 #include "ros/ros.h"
 #include "nav_msgs/Path.h"
+#include "geometry_msgs/PoseStamped.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "rrt_node");
   ros::NodeHandle n("");
   ros::Publisher path_pub = n.advertise<nav_msgs::Path>("/path", 1);
+
   Point start, goal;
   start.x = 0.0;
   start.y = 0.0;
@@ -14,7 +16,6 @@ int main(int argc, char **argv)
   goal.y = 10.0;
   RRT rrtPlanner(start, goal, 0.2);
   rrtPlanner.plan();
-  std::cout << "1111" << std::endl;
   rrtPlanner.broadcastPath();
   nav_msgs::Path path;
   path.header.frame_id = "map";
@@ -22,17 +23,19 @@ int main(int argc, char **argv)
   std::cout << LOG << "rrtPlanner._path.size() = " << rrtPlanner._path.size() << std::endl;
   for(short i = 0; i < rrtPlanner._path.size(); i++)
   {
-    std::cout << "i = " << i << std::endl;
-    path.poses[i].pose.position.x = rrtPlanner._path[i].x;
-    path.poses[i].pose.position.y = rrtPlanner._path[i].y;
+    geometry_msgs::PoseStamped p;
+    p.header.frame_id = "map";
+    p.pose.position.x = rrtPlanner._path[i].x;
+    p.pose.position.y = rrtPlanner._path[i].y;
+    path.poses.push_back(p);
 
   }
-  std::cout << LOG <<"2222" << std::endl;
-  path_pub.publish(path);
-  std::cout << LOG <<"3333" << std::endl;
-  while(1)
+  ros::Rate loop_rate(1);
+  while (ros::ok())
   {
-    sleep(2);
+    std::cout << LOG <<"pub path" << std::endl;
+    path_pub.publish(path);
+    loop_rate.sleep();
   }
   return 0;
 }
